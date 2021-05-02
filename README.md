32529a98cd292e68d80b2eb35a3ed15a933f2e58
Nikhil Gautam <nikhilnn@blah.com>
2021-05-02 11:53:41 +0530

    readme: update readme file

--- README.md
+++ README.md
@@ -1,27 +1,44 @@
-# MovableBoxGenerator
 
+#  Movable Box Generator
+![enter image description here](https://i.postimg.cc/zBfKkFYr/mov-box-gen.jpg)
+---
+ 
+This app creates boxes which can be moved in inside a container using keyboard keys. 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.11.
 
-## Development server
+ ## Cloning Repo
+ Clone the repo using below commands:
+```
+	git clone https://github.com/nikhilgautam21/movable-box-generator.git
+	cd movable-box-generator
+```
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+##  Development server
 
-## Code scaffolding
+Run ``` npm install``` to install the required packages.
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. 
 
-## Build
+  
+
+##  Build
+
+  
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
+  
+
+##  Running unit tests
+
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
+  
+
+##  Running end-to-end tests
+
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+ 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,126 @@ import { Component, OnInit, HostListener} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   boxes = [];
+  selectedBox:any;
+  selectedBoxId:number;
+  moveStep:number = 25;
+  isKeyboardEventOn:boolean = true;
+  intialBoxNumber:number = 15;
 
   constructor(){
 
+    for(let i=0;i<this.intialBoxNumber;i++){
+      this.handleAddBox();
+    }
+
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if(this.selectedBox && this.isKeyboardEventOn){
+      switch(event.code){
+        case 'KeyW':
+          this.moveBoxUp();
+          break;
+        case 'KeyS':
+          this.moveBoxDown();
+          break;
+        case 'KeyA':
+          this.moveBoxLeft();
+          break;
+        case 'KeyD':
+          this.moveBoxRight();
+          break;
+        case 'Delete':
+          this.handleDelete();
+          break;
+      }
+    }
   }
 
   ngOnInit(){
 
+  }
+
+  handleAddBox(){
+    let id = (new Date()).getTime();
+    this.boxes.push(id)
+  }
+
+  handleRemoveAll(){
+    this.boxes = []
+    this.selectedBox = null;
+    this.selectedBoxId = null;
+  }
+
+  handleBoxClick(id:number){
+    if(this.selectedBoxId == id){
+      this.selectedBoxId = null;
+      this.selectedBox = null;
+    }
+    else{
+      this.selectedBoxId = id;
+      this.selectedBox = document.getElementById(this.selectedBoxId.toString());
+    }
+  }
+
+  moveBoxUp(){
+    let boxStyle = this.selectedBox.style;
+    let selectedTop = boxStyle.top.split("px")[0];
+    if(!this.isOutOfBox('top')){
+      this.selectedBox.style.top = `${Number(selectedTop) - this.moveStep}px`;
+    }
+  }
+
+  moveBoxDown(){
+    let boxStyle = this.selectedBox.style;
+    let selectedTop = boxStyle.top.split("px")[0];
+    if(!this.isOutOfBox('bottom')){
+      this.selectedBox.style.top = `${Number(selectedTop) + this.moveStep}px`;
+    }
+  }
+
+  moveBoxLeft(){
+    let boxStyle = this.selectedBox.style;
+    let selectedLeft = boxStyle.left.split("px")[0];
+    if(!this.isOutOfBox('left')){
+      this.selectedBox.style.left = `${Number(selectedLeft) - this.moveStep}px`;
+    }
+  }
+
+  moveBoxRight(){
+    let boxStyle = this.selectedBox.style;
+    let selectedLeft = boxStyle.left.split("px")[0];
+    if(!this.isOutOfBox('right')){
+      this.selectedBox.style.left = `${Number(selectedLeft) + this.moveStep}px`;
+    }
+  }
+
+  handleDelete(){
+    this.selectedBoxId = null;
+    this.selectedBox.remove();
+  }
+
+  handleToggleKeyboardEvent(event){
+    this.isKeyboardEventOn = !this.isKeyboardEventOn
+  }
+
+  isOutOfBox(pos){
+    let wrapper = document.getElementById('box-container');
+    let wrapperCord = wrapper.getBoundingClientRect();
+    let elemCord = this.selectedBox.getBoundingClientRect();
+    switch(pos){
+      case "top":
+       return ((elemCord.top - this.moveStep) < wrapperCord.top); 
+      case "bottom":
+       return ((elemCord.top + this.moveStep) > (wrapperCord.bottom-elemCord.height)); 
+      case "left":
+       return ((elemCord.left - this.moveStep) < wrapperCord.left); 
+      case "right":
+       return ((elemCord.left + this.moveStep) > (wrapperCord.right-elemCord.height)); 
+    }
   }
 }
